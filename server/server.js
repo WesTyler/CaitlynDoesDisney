b902b6937e1663ed6ac13277f7088083a4b069fb
WesTyler <westyler5@gmail.com>
2015-10-02 18:07:58 -0500

    Work on resolving issues serving static Client/ directory with express.static

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname+'/client'));
+app.use(express.static(path.join(__dirname+'/../client')));
+console.log(path.join(__dirname+'/../client'));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('Server running on port 3000')
