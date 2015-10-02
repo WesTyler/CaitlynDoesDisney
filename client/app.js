741b70100274dacc17889ef8f10bf13452f13c74
WesTyler <westyler5@gmail.com>
2015-10-02 16:47:45 -0500

    Set up initial UI-Router routes

--- client/app.js
+++ client/app.js
@@ -1,1 +1,16 @@
-app.js
+angular.module('app', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  .state('app', {
+    url: '/',
+    templateUrl: './index.html',
+    views: {
+      'app.nav': {
+        abstract: true,
+        templateUrl: './app/nav/nav.html',
+        controller: 'NavController'
+      }
+    }
+  })
+})
