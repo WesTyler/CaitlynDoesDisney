8a7effa7eeef49343d84cf14e3530d3cc6971d43
WesTyler <westyler5@gmail.com>
2015-10-14 19:15:28 -0500

    Initial commit for AboutController and ContactController.

--- client/app.js
+++ client/app.js
@@ -2,39 +2,24 @@ var myApp = angular.module('myApp', ['ui.router'])
 myApp.config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /
-  $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise('/');
   //
   // Now set up the states
   $stateProvider
     .state('home', {
-      url: "/",
-      templateUrl: "home/home.html",
+      url: '/',
+      templateUrl: 'home/home.html',
       controller: 'HomeController'
     })
-    .state('home.list', {
-      url: "/list",
-      templateUrl: "home/home.list.html",
-    })
     .state('about', {
-      url: "/about",
-      templateUrl: "about/about.html"
-    })
-    .state('about.list', {
-      url: "/list",
-      templateUrl: "about/about.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Second", "Set", "Of", "Things"];
-      }
+      url: '/about',
+      templateUrl: 'about/about.html',
+      controller: 'AboutController'
     })
     .state('contact', {
-      url: "/contact",
-      templateUrl: "contact/contact.html"
+      url: '/contact',
+      templateUrl: 'contact/contact.html',
+      controller: 'ContactController'
     })
-    .state('contact.list', {
-      url: "/list",
-      templateUrl: "contact/contact.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Third", "Set", "Of", "Things"];
-      }
     });
 })
