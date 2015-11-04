a5e78211f79076f11b6315e3bfb8c24af9784c5f
WesTyler <westyler5@gmail.com>
2015-11-03 22:56:08 -0600

    Define kanban column and card structure, set up getter functions

--- client/workflow/workflow.js
+++ client/workflow/workflow.js
@@ -1,10 +1,30 @@
 angular.module('myApp')
-.controller('WorkflowController', ['$scope', function($scope){
-  $scope.columns = {'quoteRequested': {label: 'Quote Requested', cards: []}, 'quoteSent': {label: 'Quote Sent', cards: []}};
-  $scope.cards = [{name: 'Tyler Family', tripDate: '05/04/2016 - 05/11/2016', destination: 'Disney World', pixie: 'None', adults: 2, kids: 0, column: 'quoteRequested'}];
+.controller('WorkflowController', ['$scope', '$http', function($scope, $http){
+  $scope.columns = {'quoteRequested': {label: 'Quote Requested', cards: []}, 'quoteSent': {label: 'Quote Sent', cards: []}, 'depositPaid': {label: 'Deposit Paid', cards: []}};
+  $scope.cards = [{name: 'Tyler Family', tripDate: '05/04/2016 - 05/11/2016', destination: 'Disney World', pixie: 'None', adults: 2, kids: 0, column: 'depositPaid'}, {name: 'Redfearn Family', tripDate: '05/04/2016 - 05/11/2016', destination: 'Disney World', pixie: 'None', adults: 2, kids: 0, column: 'quoteSent'}];
   $scope.cards.forEach(function(card) {
-    console.log(card.column, $scope.columns[card.column])
     $scope.columns[card.column].cards.push(card);
   })
-  console.log('ctrl loaded')
+
+  $scope.getColumns = function() {
+    console.log('*** workflow.js: Getting Columns ***');
+    $http({
+      method: 'GET',
+      url: '/columns'
+    })
+    .then(function(response) {
+      $scope.columns = response;
+    });
+  };
+
+  $scope.getCards = function() {
+    console.log('*** workflow.js: Getting Cards ***');
+    $http({
+      method: 'GET',
+      url: '/cards'
+    })
+    .then(function(response) {
+      $scope.cards = response;
+    });
+  };
 }])
