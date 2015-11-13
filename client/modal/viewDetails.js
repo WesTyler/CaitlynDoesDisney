794971eb4580273a47fde8085b3253f7f3cd57cb
WesTyler <westyler5@gmail.com>
2015-11-13 12:37:50 -0600

    Add edit functionality to modal fields, tied to card model

--- client/modal/viewDetails.js
+++ client/modal/viewDetails.js
@@ -1,10 +1,24 @@
 angular.module('myApp')
 .controller('ViewDetailsController', ['DataService', '$scope', '$rootScope', '$state', function(DataService, $scope, $rootScope, $state){
   $scope.card = DataService.getData();
+  $scope.edit = {};
 
   $scope.saveAndExit = function() {
     DataService.setData($scope.card);
     $rootScope.$broadcast('CardUpdated');
     $state.go('workflow');
-  }  
+  };
+
+  $scope.editField = function(fieldName, subField) {
+    $scope.edit[fieldName] = true;
+    if (subField) {
+      $scope.edit[fieldName] = {};
+      $scope.edit[fieldName][subField] = true;
+    }
+  };
+
+  $scope.saveField = function(fieldName) {
+    console.log('*** \n', fieldName, subfield)
+    delete $scope.edit[fieldName];
+  };
 }]);
